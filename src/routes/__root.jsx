4647c5399a153f529774a523dc1d36b7d57b3871
liuhu016 <liuhu016@realsee.com>
2025-11-09 20:48:24 +0800

    feat:update

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,11 +1,32 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { Layout, Menu } from "antd";
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+const { Header, Footer, Content } = Layout;
 
+const headerStyle = {
+  textAlign: 'center',
+  height: 40,
+  lineHeight: '40px',
+  backgroundColor: 'transparent',
+};
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Outlet />
-      <TanStackRouterDevtools />
+    <Layout>
+      <Header style={headerStyle}>
+        <Menu  mode="horizontal" items={[
+          { key: 'mail', icon: <MailOutlined />, label: <Link to="/">Home</Link> },
+          { key: 'app', icon: <AppstoreOutlined />, label: <Link to="/about">About</Link> },
+        ]} />;
+      </Header>
+      <Content>
+         <Outlet />
+         <TanStackRouterDevtools />
+      </Content>
+    </Layout>
+     
+      
     </>
   ),
 })
